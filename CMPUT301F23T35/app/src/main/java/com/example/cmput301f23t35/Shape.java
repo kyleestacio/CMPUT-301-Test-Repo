6553159fba0666dccf866916c1dc00d6ee20930e
Kyle <kylemestacio@gmail.com>
2023-10-08 19:00:02 -0600

    first commit

---
+++ CMPUT301F23T35/app/src/main/java/com/example/cmput301f23t35/Shape.java
@@ -0,0 +1,6 @@
+package com.example.cmput301f23t35;
+
+public abstract class Shape {
+    int x;
+    int y;
+}
---
+++ CMPUT301F23T35/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT301F23T35"
+include(":app")
+ 
